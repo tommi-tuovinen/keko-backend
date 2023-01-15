acfb6d0eb56a5e691da5fdec1ad3cfc55bd5f03b
Tommi Tuovinen <tommi.s.tuovinen@gmail.com>
2023-01-15 21:05:58 +0200

    Configure Swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
+
+
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Keko API",
+        Description = "A .NET 7 minimal API for managing ToDo items"
+    });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
