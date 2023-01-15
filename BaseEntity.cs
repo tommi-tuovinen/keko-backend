2804661e0fa9b9c6caef318564a845e6258a5e7e
Tommi Tuovinen <tommi.s.tuovinen@gmail.com>
2023-01-15 21:50:01 +0200

    Add TaskEntity

---
+++ BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace Keko.Data;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public string ModifiedBy { get; set; }
+}
---
+++ TaskEntity.cs
@@ -0,0 +1,7 @@
+namespace Keko.Data;
+
+public class TaskEntity : BaseEntity
+{
+    public string Name { get; set; }
+    public string Priority { get; set; }
+}
